ebfcd42a9738acf06919a3085792c9af7dff1c4f
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-09-29 14:38:50 +0300

    fe done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import axios from 'axios'
+import Card from '../src/components/Card'
 
 function App() {
+  const [burgers, setBurgers] = useState(null)
+
+  const fetchData = async () => {
+    const burgerData = await axios.get('http://localhost:3001/burgers')
+    const data = Object.keys(burgerData.data.data).map(burger => burgerData.data.data[burger])
+    setBurgers(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
+      <h1>Favorite Burgers</h1>
+
+      <div>
+        {burgers?.map(burger => <Card key={burger.id} burger={burger}/>)}
+      </div>
     </div>
   );
 }
